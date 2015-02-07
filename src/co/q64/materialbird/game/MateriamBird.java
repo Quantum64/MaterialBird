33070e9e7a7778045acc566e6ffcd75198925512
Dylan Ryman <dylan@dtr-world.com>
2015-02-06 23:28:46 -0500

    Base game classes

--- src/co/q64/materialbird/game/MateriamBird.java
+++ src/co/q64/materialbird/game/MateriamBird.java
@@ -1,5 +1,13 @@
 package co.q64.materialbird.game;
 
-public class MateriamBird {
+import co.q64.materialbird.framework.Screen;
+import co.q64.materialbird.framework.implementation.AndroidGame;
 
+public class MateriamBird extends AndroidGame {
+
+	@Override
+	public Screen getInitScreen() {
+		return null;
+	}
+	
 }
---
+++ src/co/q64/materialbird/game/screens/SplashScreen.java
@@ -0,0 +1,48 @@
+package co.q64.materialbird.game.screens;
+
+import co.q64.materialbird.framework.Game;
+import co.q64.materialbird.framework.Screen;
+
+public class SplashScreen extends Screen {
+
+	public SplashScreen(Game game) {
+		super(game);
+	}
+
+	@Override
+	public void update(float deltaTime) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void paint(float deltaTime) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void backButton() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
