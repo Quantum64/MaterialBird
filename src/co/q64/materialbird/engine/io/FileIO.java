35b1396ed60973198cd185b6a705d30bf227563a
Dylan Ryman <dylan@dtr-world.com>
2015-02-09 18:49:31 -0500

    Move redundant code to abstract class

--- src/co/q64/materialbird/engine/io/FileIO.java
+++ src/co/q64/materialbird/engine/io/FileIO.java
@@ -1,46 +1,11 @@
 package co.q64.materialbird.engine.io;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-
 import android.content.Context;
-import android.content.SharedPreferences;
-import android.content.res.AssetManager;
-import android.os.Environment;
-import android.preference.PreferenceManager;
-import co.q64.materialbird.engine.interfaces.IFileIO;
 
-public class FileIO implements IFileIO {
-	private Context context;
-	private AssetManager assets;
-	private String externalStoragePath;
+public class FileIO extends BasicFileIO {
 
 	protected FileIO(Context context) {
-		this.context = context;
-		this.assets = context.getAssets();
-		this.externalStoragePath = Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator;
+		super(context);
 	}
-
-	@Override
-	public InputStream readAsset(String file) throws IOException {
-		return assets.open(file);
-	}
-
-	@Override
-	public InputStream readFile(String file) throws IOException {
-		return new FileInputStream(externalStoragePath + file);
-	}
-
-	@Override
-	public OutputStream writeFile(String file) throws IOException {
-		return new FileOutputStream(externalStoragePath + file);
-	}
-
-	public SharedPreferences getSharedPref() {
-		return PreferenceManager.getDefaultSharedPreferences(context);
-	}
+	
 }
